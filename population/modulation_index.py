2d59dce6bd811053b8492b4598ad24a7fcc9f757
Susu <amsi.1994@gmail.com>
2023-09-08 18:17:27 -0400

    changes to modulation_index

--- population/modulation_index.py
+++ population/modulation_index.py
@@ -4,44 +4,96 @@ from matplotlib import pyplot as plt
 
 from scipy.stats import mannwhitneyu
 
-def modulation_index(population_data_path, metadata_file, movies, data_paths, genotype,
-                     psth_type = 'spike_rate', metric = 'trial_type_selectivity', method = 'mann_whiteney',
+def modulation_index(activity_dict, epoch_start_timepoints, epoch_end_timepoints,
+                     metric = 'trial_type_selectivity', method = 'mann_whiteney',
+                     trial_types = ['Left', 'Right'], trial_epochs = ['Pre-sample', 'Sample', 'Delay', 'Response'],
+                     baseline_epoch = 'Pre-sample',
                      plot_example_neurons = 'False'):
 
-    psth = population_psth.get_population_psth(population_data_path, movies, data_paths, metadata_file, genotype,
-                                                 make_plot_spike_psth = False, make_plot_dFF_psth = False, plot_psths = False)
-    if psth_type == 'spike_rate':
-        psth = psth['spikes']
-        left_trials_activity = psth['spike_count_trials_left']
-        right_trials_activity = psth['spike_count_trials_right']
-    else:
-        if psth_type == 'dFF':
-            psth = psth['dFF']
-        else:
-            print('\'psth_type\' must be either \'spike_rate\' or \'dFF\'')
-            return
+    """ Calculate and plot the trial type-selectivity or trial epoch-selectivity index over all neurons in the population.
 
-    n_cells = len(left_trials_activity.keys())
-    assert(len(right_trials_activity.keys()) == n_cells)
+        Inputs:
+        activity_dict: Dictionary with a key-value pair for each trial type, itself containing a dictionary with keys 0, 1, 2... n_cells
+                       with a n_trials X n_timepoints activity matrix for each cell
+        epoch_start_timepoints: list of starting timepoint for each epoch. Should be equal in length to trial_epochs
+        epoch_end_timepoints: list of ending timepoint for each epoch. Should be equal in length to trial_epochs
+        metric: String, 'trial_type_selectivity' (default), 'trial_epoch_selectivity' or 'tuning_curve_variance'.
+        method: String, 'mann_whiteney' (default) or 'd-prime' - method for comparing activity in trial types/epochs.
+        trial_types: list of trial types, should match activity_dict.keys(). Currently, only two trial types are supported.
+        trial_epochs: list of trial_epochs. Should be equal in length to epoch_start_timepoints
+        baseline_epoch: String, one of the elements of trial_epochs. Used as a comparison for calculating trial epoch modulation index
+
+        Output:
+        mod_ind: Dictionary with a key-value pair for each trial epoch, itself containing:
+                 - if metric is 'trial_type_selectivity': n_cells X 1 vector with modulation index for each cell
+                 - if metric is 'trial_epoch_selectivity': n_cells X len(trial_types) matrix with modulation index for each cell and each trial type.
+                                                           In this case, mod_ind[baseline_epoch] is a matrix of zeros.
+
+    """
+    n_epochs = len(trial_epochs)
+    assert(len(epoch_start_timepoints) == n_epochs)
+    assert(len(epoch_end_timepoints) == n_epochs)
+
+    n_types = len(trial_types)
+    assert(n_types == 2)
+    assert(np.all(list(activity_dict.keys())) == trial_types)
+
+    n_cells = len(activity_dict[trial_types[0]].keys())
+    for type in trial_types:
+        assert(len(activity_dict[type].keys()) == n_cells)
+
+    n_timepoints = activity_dict[type][0].shape[1]
+    assert(n_timepoints > np.max(epoch_start_timepoints))
+
+    assert(baseline_epoch in trial_epochs)
+    baseline_epoch_no = trial_epochs.index(baseline_epoch)
 
     if metric == 'trial_type_selectivity':
-        mod_ind = np.zeros([n_cells, 4])
+        mod_ind = {epoch: np.zeros(n_cells) for epoch in trial_epochs}
         for cell in range(n_cells):
-            for i in range(4):
-                    mod_ind[cell, i] = compare_activity(left_trials_activity[cell][:, i], right_trials_activity[cell][:, i],
-                                            method)
+            for e in range(n_epochs):
+
+                    t1 = epoch_start_timepoints[e]
+                    t2 = epoch_end_timepoints[e]
+
+                    v1 = np.mean(activity_dict[trial_types[0]][cell][:, t1:t2], axis = 1)
+                    assert(len(v1) == activity_dict[trial_types[0]][cell].shape[0])
+                    v2 = np.mean(activity_dict[trial_types[1]][cell][:, t1:t2], axis = 1)
+                    assert(len(v2) == activity_dict[trial_types[1]][cell].shape[0])
+
+                    mod_ind[epochs[e]][cell] = compare_activity(v1, v2, method)
     else:
-        if metric == 'epoch_selectivity':
-            mod_ind = np.zeros([n_cells, 4])
+        if metric == 'trial_epoch_selectivity':
+            mod_ind = {epoch: np.zeros([n_cells, n_types]) for epoch in trial_epochs}
             for cell in range(n_cells):
-                for i in range(1, 4):
-                    pre_sample_vec = np.concatenate([left_trials_activity[cell][:, 0],
-                                                    right_trials_activity[cell][:, 0]], axis = 0)
-                    assert(len(pre_sample_vec) == left_trials_activity[cell].shape[0] + right_trials_activity[cell].shape[0])
-                    epoch_vec = np.concatenate([left_trials_activity[cell][:, i],
-                                                    right_trials_activity[cell][:, i]], axis = 0)
-                    assert(len(epoch_vec) == left_trials_activity[cell].shape[0] + right_trials_activity[cell].shape[0])
-                    mod_ind[cell, i] = compare_activity(epoch_vec, pre_sample_vec, method)
+
+                # Baseline epoch activity for trial type 1
+                vb1 = np.mean(activity_dict[trial_types[0]][cell][:, epoch_start_timepoints[baseline_epoch_no]:epoch_end_timepoints[baseline_epoch_no]], axis = 1)
+                assert(len(vb1) == activity_dict[trial_types[0]][cell].shape[0])
+
+                # Baseline epoch activity for trial type 2
+                vb2 = np.mean(activity_dict[trial_types[1]][cell][:, epoch_start_timepoints[baseline_epoch_no]:epoch_end_timepoints[baseline_epoch_no]], axis = 1)
+                assert(len(vb2) == activity_dict[trial_types[1]][cell].shape[0])
+
+                for e in range(n_epochs):
+
+                    if not e == baseline_epoch_no:
+
+                        t1 = epoch_start_timepoints[e]
+                        t2 = epoch_end_timepoints[e]
+
+                        # Modulation index for trial type 1
+                        v1 = np.mean(activity_dict[trial_types[0]][cell][:, t1:t2], axis = 1)
+                        assert(len(v1) == activity_dict[trial_types[0]][cell].shape[0])
+
+                        mod_ind[epoch][cell, 0] = compare_activity(vb1, v1, method)
+
+                        # Modulation index for trial type 2
+                        v2 = np.mean(activity_dict[trial_types[1]][cell][:, t1:t2], axis = 1)
+                        assert(len(v2) == activity_dict[trial_types[1]][cell].shape[0])
+
+                        mod_ind[epoch][cell, 1] = compare_activity(vb2, v2, method)
+
         else:
             if metric == 'tuning_curve_variance':
                 mod_ind = np.zeros([n_cells])
@@ -77,24 +129,26 @@ def modulation_index(population_data_path, metadata_file, movies, data_paths, ge
                 print('\'metric\' must be one of the following:\n\'trial_type_selectivity\'\n\'epoch_selectivity\'\n\'tuning_curve_variance\'')
     return mod_ind
 
-def compare_activity(left, right, method = 'mann_whitney', pval_threshold = 0.05):
+def compare_activity(v1, v2, method = 'mann_whiteney', pval_threshold = 0.05):
+    """ Compare two activity vectors v1 or v2 using mann-mann_whiteney U test or d prime.
+    """
 
     if method == 'mann_whiteney':
         try:
-            pval = mannwhitneyu(left, right, alternative = 'two-sided')[1]
+            pval = mannwhitneyu(v1, v2, alternative = 'two-sided')[1]
         except ValueError as e:
             print(e)
             pval = 1
         return pval < pval_threshold
     else:
         if method == 'd-prime':
-            if np.std(left) + np.std(right) == 0:
-                if np.mean(left) == np.mean(right):
+            if np.std(v1) + np.std(v2) == 0:
+                if np.mean(v1) == np.mean(v2):
                     d_prime = 0
                 else:
                     print('Unequal means with zero variance')
             else:
-                d_prime = np.divide(np.mean(left) - np.mean(right), (np.std(left) + np.std(right))/2)
+                d_prime = np.divide(np.mean(v1) - np.mean(v2), (np.std(v1) + np.std(v2))/2)
             return d_prime
         else:
             print('\'method\' must be \'mann_whitney\' or \'d-prime\'')
